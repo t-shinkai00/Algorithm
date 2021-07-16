a2b93d3d24703d24c3434931804878ea8e9b9641
t-shinkai00 <t.shinkai00@gmail.com>
2021-07-16 16:36:55 +0900

    add binary search

---
+++ binary-search.py
@@ -0,0 +1,20 @@
+input=int(input())
+# print(input)
+A=[12,15,19,36,38,45,56,88,99]
+# print(len(A))
+# half=int(len(A)/2)
+# print(A[:half])
+# print(A[half:])
+def BinarySearch(list,b):
+    n=len(list)
+    half=int(n/2)
+    print(n)
+    if list[half]==b:
+        return True
+    elif n==1:
+        return False
+    elif list[half]>b:
+        return BinarySearch(list[:half],b)
+    else:
+        return BinarySearch(list[half:],b)
+print(BinarySearch(A,input))
