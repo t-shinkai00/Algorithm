50955eb28224cde06dd34802f9f04d3712c7f03e
t-shinkai00 <t.shinkai00@gmail.com>
2021-09-06 23:35:17 +0900

    update

--- ITP1-8-A.py
+++ ITP1-8-A.py
@@ -1,5 +1,5 @@
 # https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/8/ITP1_8_A
-line=list(map(str,input()))
+line=list(input())
 for i in range(len(line)):
     if line[i].isupper():
         line[i]=line[i].lower()
