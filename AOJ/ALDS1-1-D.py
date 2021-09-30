ea386e7d24104e4c9f7a71688c2c963d945ac020
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-01 00:17:16 +0900

    update

--- AOJ/ALDS1-1-D.py
+++ AOJ/ALDS1-1-D.py
@@ -1,6 +1,9 @@
 # https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_1_D
 n=int(input())
-R=[]
-for i in range(n):
-    R.append(int(input()))
-print(R)
+minv=int(input())
+maxv=-10**12
+for i in range(n-1):
+    r=int(input())
+    maxv=max(maxv,r-minv)
+    minv=min(minv,r)
+print(maxv)
