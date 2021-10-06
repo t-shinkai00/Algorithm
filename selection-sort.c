27cf877aabc2ef051a44b5278064e3808154d708
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-07 02:27:44 +0900

    add selection-sort

---
+++ selection-sort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void selection_sort(int n, int a[]){
+  int i, j, k, m, t;
+  for(i = 0; i < n-1; i++){
+    for(t = 0; t < n; t++)
+      printf("%d ", a[t]);
+    printf("\n");
+    m = a[i]; k = i;
+    for(j = i+1; j < n; j++){
+      if(a[j] < m){
+        m = a[j]; k = j;
+      }
+    }
+    a[k] = a[i]; a[i] = m;
+  }
+}
+
+int main(void){
+  int arr[5]={3,5,2,1,4};
+  selection_sort(5,arr);
+  return 0;
+}
