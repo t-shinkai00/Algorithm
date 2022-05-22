45145c3425ab7740c42803dd49503ec6a19474ba
t-shinkai00 <t.shinkai00@gmail.com>
2022-05-22 21:15:52 +0900

    add

---
+++ HackerRank/1WeekPreparationKit/3-1.py
@@ -0,0 +1,23 @@
+def findZigZagSequence(a, n):
+  a.sort()
+  mid = int((n + 1)/2)
+  a[mid-1], a[n-1] = a[n-1], a[mid-1]
+
+  st = mid
+  ed = n - 2
+  while(st < ed):
+    a[st], a[ed] = a[ed], a[st]
+    st += 1
+    ed -= 1
+
+  for i in range(n):
+    if i == n-1:
+      print(a[i])
+    else:
+      print(a[i], end = ' ')
+
+test_cases = int(input())
+for cs in range (test_cases):
+  n = int(input())
+  a = list(map(int, input().split()))
+  findZigZagSequence(a, n)
