1d34d17b1001722e3c318501571f0ecc9199b63d
t-shinkai00 <t.shinkai00@gmail.com>
2022-11-12 23:01:32 +0900

    add: code

---
+++ AtCoder/abc277/B.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+S = []
+str1 = ["H", "D", "C", "S"]
+str2 = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"]
+ans = "Yes"
+for _ in range(N):
+  line = input()
+  if line in S or line[0] not in str1 or line[1] not in str2:
+    ans = "No"
+    break
+  else:
+    S.append(line)
+
+print(ans)
